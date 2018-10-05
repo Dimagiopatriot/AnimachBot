6c8b094f664d21302dca428a3f25ffcdc794aa90
Dmitriy Smishniy <troll3758@gmail.com>
2018-10-05 15:03:25 +0300

    add tests

--- src/test/kotlin/RestTest.kt
+++ src/test/kotlin/RestTest.kt
@@ -1,4 +1,5 @@
 import deviantart.rest.Rest
+import deviantart.rest.RestManager
 import org.junit.Test
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.TimeUnit
@@ -6,15 +7,41 @@ import java.util.concurrent.TimeUnit
 
 class RestTest {
 
-    private val lock = CountDownLatch(1)
-
     @Test
     fun testUserGallery() {
         val rest = Rest()
+        val lock = CountDownLatch(1)
         rest.getUserGallery("chubymi", { response ->
             print(response)
             lock.countDown()
         })
+
+        lock.await(30000, TimeUnit.MILLISECONDS)
+    }
+
+    @Test
+    fun testRestManagerPictureUrlFroUserGalleryContentNull() {
+        val restManager = RestManager()
+        val lock = CountDownLatch(1)
+
+
+        restManager.getPictureURLFromUserGallery("johnsu", 412, 61) { response ->
+            println(response)
+            lock.countDown()
+        }
+
+        lock.await(30000, TimeUnit.MILLISECONDS)
+    }
+
+    @Test
+    fun testRestManagerRandomPicRandomUser() {
+        val restManager = RestManager()
+        val lock = CountDownLatch(1)
+
+        restManager.getRamdomPicFromRandomAuthor { response ->
+            println(response)
+            lock.countDown()
+        }
 
         lock.await(30000, TimeUnit.MILLISECONDS)
     }
