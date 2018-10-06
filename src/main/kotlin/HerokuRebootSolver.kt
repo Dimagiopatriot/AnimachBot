3f39da7fdf8c4a02eee0885c646dbef48030afd5
Dmitriy Smishniy <troll3758@gmail.com>
2018-10-06 18:58:19 +0300

    Heroku 90 sec resolver v3

--- src/main/kotlin/HerokuRebootSolver.kt
+++ src/main/kotlin/HerokuRebootSolver.kt
@@ -1,15 +1,7 @@
-import telegram.Bot
-import java.io.File
 
 fun restartApplicationOnTimeout() {
-    val javaBin = System.getProperty("java.home") + File.separator + "bin" + File.separator + "java"
-    val currentJar = File(Bot::class.java.protectionDomain.codeSource.location.toURI())
-
-    if (!currentJar.name.endsWith(".jar"))
-        return
-    val command = listOf(javaBin, "-jar", currentJar.path)
-    val builder = ProcessBuilder(command)
-    builder.start()
+    val command = ("java -jar build/libs/AnimachBot-1.0-SNAPSHOT.jar")
+    Runtime.getRuntime().exec(command)
     System.exit(0)
 
 }
