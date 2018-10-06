89bf520b6e21d2aa9077701566b3fdc9f83e3610
Dmitriy Smishniy <troll3758@gmail.com>
2018-10-06 18:30:13 +0300

    Heroku 90 sec resolver v2

--- src/main/kotlin/HerokuRebootSolver.kt
+++ src/main/kotlin/HerokuRebootSolver.kt
@@ -1,6 +1,15 @@
+import telegram.Bot
+import java.io.File
 
 fun restartApplicationOnTimeout() {
-    val command = "heroku restart"
-    Runtime.getRuntime().exec(command)
+    val javaBin = System.getProperty("java.home") + File.separator + "bin" + File.separator + "java"
+    val currentJar = File(Bot::class.java.protectionDomain.codeSource.location.toURI())
+
+    if (!currentJar.name.endsWith(".jar"))
+        return
+    val command = listOf(javaBin, "-jar", currentJar.path)
+    val builder = ProcessBuilder(command)
+    builder.start()
     System.exit(0)
+
 }
