7cdcb53babe66f6329b69ac7de29825a9c0f3bff
Dmitriy Smishniy <troll3758@gmail.com>
2018-10-06 21:08:09 +0300

    remove unused classes and methods

--- src/main/kotlin/HerokuRebootSolver.kt
+++
@@ -1,7 +0,0 @@
-
-fun restartApplicationOnTimeout() {
-    val command = ("web: java -jar build/libs/AnimachBot-1.0-SNAPSHOT.jar")
-    Runtime.getRuntime().exec(command)
-    System.exit(0)
-
-}
--- src/main/kotlin/telegram/Application.kt
+++ src/main/kotlin/telegram/Application.kt
@@ -3,10 +3,6 @@ package telegram
 import org.telegram.telegrambots.ApiContextInitializer
 import org.telegram.telegrambots.meta.TelegramBotsApi
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException
-import restartApplicationOnTimeout
-import java.util.*
-
-const val RESTART_TIME = 30000
 
 fun main(args: Array<String>) {
     ApiContextInitializer.init()
@@ -17,14 +13,4 @@ fun main(args: Array<String>) {
     } catch (e: TelegramApiException) {
         e.stackTrace
     }
-    //Timer in new thread
-   /* Thread(Runnable {
-        val currentTime = Calendar.getInstance().timeInMillis
-        while (true) {
-            if (RESTART_TIME < Calendar.getInstance().timeInMillis - currentTime) {
-                restartApplicationOnTimeout()
-            }
-
-        }
-    }).start()*/
 }
